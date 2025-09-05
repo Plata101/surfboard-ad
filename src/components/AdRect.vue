e0d7325812bdc20d78d8835b3fb1abe0cf72e1a8
Plata101 <marc.friedmann@yahoo.com>
2025-09-05 11:40:56 +0200

    Add Open Sans font integration, Build reusable AdRect component with CTA

---
+++ src/components/AdRect.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  title: String,
+  imageSrc: String,
+  imageAlt: String,
+  buttonText: String,
+});
+</script>
+
+<template>
+  <div class="w-[300px] h-auto bg-white shadow-2xl pt-6">
+    <p class="text-xl text-center font-semibold font-open-sans">
+      {{ title }}
+    </p>
+    <figure class="w-full px-6 my-6">
+      <img
+        :src="imageSrc"
+        :alt="imageAlt"
+        class="w-full h-full object-contain"
+      />
+    </figure>
+    <button class="text-center text-white bg-[#FEA300] py-2 text-lg w-full">
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
